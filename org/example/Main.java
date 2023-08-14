0308820e4251eb813e443a0f6bf9b4163e730e01
yuu14mm <124451437+yuu14mm@users.noreply.github.com>
2023-08-14 22:20:08 +0900

    第4回課題
    .forEach(name -> System.out.println(name));をメソッド参照に変更

--- org/example/Main.java
+++ org/example/Main.java
@@ -7,14 +7,14 @@ public class Main {
 
         //音楽のジャンルを格納
         List<String> music = List.of("rock", "jazz", "classic", "pops");
-        
+
         music.stream()
                 //'o'が含まれる音楽のジャンルを抽出
                 .filter(name -> name.contains("o"))
                 //大文字に変換
                 .map(name -> name.toUpperCase())
                 //変換したデータを出力
-                .forEach(name -> System.out.println(name));
+                .forEach(System.out::println);
     }
 }
 
